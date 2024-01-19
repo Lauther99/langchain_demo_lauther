38c1ea119a40859ce9081d56456be799959ffba4
Lauther <lautherharold27@gmail.com>
2024-01-19 14:09:30 -0500

    feat: improved sql_translator instructions

--- demos/tools/sql_translator/instructions.py
+++ demos/tools/sql_translator/instructions.py
@@ -1,13 +1,13 @@
 system_instruction = """You are a text-to-SQL translator. You write Microsoft SQL Server 2014 code based on plain-language prompts.
 Your only output always must be SQL code. Do not include any other text. ONLY SQL CODE."""
 
-sql_translator_instruction = """Translate '{query}' to a syntactically-correct Microsoft SQL Server 2014 query"""
+sql_translator_instruction = """Translate '{query}' to a syntactically-correct Microsoft SQL Server 2014 query, DO NOT FORGET to include SQL SCHEMA 'dbo_v2' for tables in your query result"""
 
 content_error_instruction = """
 Your last code gives me this error: '''{error_message}'''.
 Follow this steps:
-First, take your time to read the descriptions to find the connection between tables and columns names from the given database information, this step is the most important DO NOT IGNORE the relations between tables.
-Second, REWRITE a new query taking STRICTLY into account the connections between tables, take your time do not answer faster
+First, take your time to read the descriptions to find the connection between tables and columns names from the given database information, this step is the most important DO NOT IGNORE the relations between tables neither the SQL SCHEMA 'dbo_v2' for tables.
+Second, REWRITE a new query taking STRICTLY into account the connections between tables.
 Third, after rewriting you have to compare STRICTLY the table and column name if there exists in the given database information one by one, take your time. Here is an example of what you have to do: If your answer is "SELECT U.NOMBRE FROM dbo_v2.users U", you have to look first for the table users in the information, if you find the table then you look for the correct column name in the information, if you find the correct column name for every table in your query, then you answer if you could not find them in the information rewrite the query again and repeat.
 Fourth, do the third step a second time. If every step was done right you answer DO NOT include any other text.
 """
